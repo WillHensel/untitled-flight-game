d6286382eaee57282b512e36db722c062e2a031e
Will Hensel <william.r.hensel@gmail.com>
2022-03-08 21:26:44 -0800

    Better flying, rain

--- app.js
+++ app.js
@@ -1,55 +1,48 @@
 var canvas = null;
 var engine = null;
 
-function createScene() {
+async function createScene() {
     let scene, camera, light, piperBody;
 
     function setupScene() {
         scene = new BABYLON.Scene(engine);
+        scene.collisionsEnabled = true;
     }
     function setupCamera() {
         console.log("At start setupCamera");
-        camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -20), scene);
+        camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 100, -20), scene);
         camera.radius = 20;
-        camera.rotationOffset = 0;
         camera.heightOffset = 1;
-        camera.cameraAcceleration = .1;
-        camera.maxCameraSpeed = 10;
+        camera.rotationOffset = 0;
+        camera.cameraAcceleration = 0.5;
+        camera.maxCameraSpeed = 100;
         camera.attachControl(canvas, true);
-        
-
+        camera.checkCollisions = true;
     }
+
     function setupLights() {
         light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(-1, -1, 0), scene);
         light.diffuse = new BABYLON.Color3(1, 1, 1);
         light.specular = new BABYLON.Color3(1, 1, 1);
-        light.intensity = 1.5;
-        light.position = new BABYLON.Vector3(0, 100, 0);
+        light.intensity = 0.5;
+        light.position = new BABYLON.Vector3(0, 100, 10);
 
-        light2 = new BABYLON.HemisphericLight("HemisphericLight", new BABYLON.Vector3(1, 1, 0), scene);
-        light2.intensity = 0.4
-    }
-    function setupOrientationLines() {
-        let xLine = BABYLON.MeshBuilder.CreateLines("xLine", {
-            points: [new BABYLON.Vector3(0, 0.01, 0), new BABYLON.Vector3(4, 0.01, 0)],
-            colors: [new BABYLON.Color4(1, 0, 0, 1), new BABYLON.Color4(1, 0, 0, 1)]
-        }, scene);
-        let yLine = BABYLON.MeshBuilder.CreateLines("yLine", {
-            points: [new BABYLON.Vector3(0, 0.01, 0), new BABYLON.Vector3(0, 4.01, 0)],
-            colors: [new BABYLON.Color4(0, 1, 0, 1), new BABYLON.Color4(0, 1, 0, 1)]
-        }, scene);
-        let zLine = BABYLON.MeshBuilder.CreateLines("zLine", {
-            points: [new BABYLON.Vector3(0, 0.01, 0), new BABYLON.Vector3(0, 0.01, 4)],
-            colors: [new BABYLON.Color4(0, 0, 1, 1), new BABYLON.Color4(0, 0, 1, 1)]
-        }, scene);
+        // light2 = new BABYLON.HemisphericLight("HemisphericLight", new BABYLON.Vector3(1, 1, 0), scene);
+        // light2.intensity = 0.1;
     }
 
-    function importMeshes() {
+    async function importAndSetupPiper() {
 
         let piperProp;
+
         let piperRutter;
         let piperAileronPilot;
         let piperAileronPass;
+        let piperPositionLightRight;
+        let piperPositionLightLeft;
+        let piperPositionLightTail;
+
+        let cameraAttachment;
 
         function setupInitialTranslations() {
             piperRutter.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
@@ -57,12 +50,33 @@ function createScene() {
             piperElevator.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
             piperAileronPilot.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
             piperAileronPass.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
+            piperPositionLightRight.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
+            piperPositionLightLeft.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
+            piperPositionLightTail.translate(new BABYLON.Vector3(1.75, -0.4, 0), 1, BABYLON.Space.WORLD);
            
             piperBody.addRotation(0, Math.PI / 2, -Math.PI / 15);
-            piperBody.translate(new BABYLON.Vector3(0, 10 + 0.9, 2), BABYLON.Space.WORLD);
+            piperBody.translate(new BABYLON.Vector3(0, 50, 2), BABYLON.Space.WORLD);
         }
 
-        function setupPropAnimation() {
+        function setupLights() {
+            let positionLightRedWing = new BABYLON.PointLight("PositionLightRedWing", piperPositionLightLeft.position, scene);
+            positionLightRedWing.diffuse = new BABYLON.Color3(0.8, 0.011, 0.008);
+            positionLightRedWing.specular = new BABYLON.Color3(0.8, 0.011, 0.008);
+
+            let positionLightRedTail = new BABYLON.PointLight("PositionLightRedTail", piperPositionLightTail.position, scene);
+            positionLightRedTail.diffuse = new BABYLON.Color3(0.8, 0.011, 0.008);
+            positionLightRedTail.specular = new BABYLON.Color3(0.8, 0.011, 0.008);
+
+            let positionLightGreen = new BABYLON.PointLight("PositionLightGreen", piperPositionLightRight.position, scene);
+            positionLightGreen.diffuse = new BABYLON.Color3(0.004, 0.8, 0.0);
+            positionLightGreen.specular = new BABYLON.Color3(0.004, 0.8, 0.0);
+
+            positionLightRedWing.parent = piperPositionLightLeft;
+            positionLightGreen.parent = piperPositionLightRight;
+            positionLightRedTail.parent = piperPositionLightTail;
+        }
+
+        function setupAnimations() {
             let rotation = 2;
             scene.registerBeforeRender(function() {
                 if (rotation > 2) {
@@ -71,19 +85,15 @@ function createScene() {
                 piperProp.rotation = new BABYLON.Vector3(rotation * Math.PI, 0, 0);
                 rotation += 0.2;
 
+                cameraAttachment.position = piperBody.position;
+
                 piperBody.translate(new BABYLON.Vector3(-1, 0, 0), 1, BABYLON.Space.LOCAL);
-                if (piperBody.rotation.x < 0) {
-                    piperBody.addRotation(0, -0.0001, -0.0001);
-                }
-                if (piperBody.rotation.x > 0) {
-                    piperBody.addRotation(0, 0.0001, -0.001);
-                }
             });
-        }
+       }
 
         function setupControlSurfaceInput() {
             let rotationOffset = Math.PI / 36;
-            let rotationLimit = Math.PI / 6;
+            let rotationLimit = Math.PI / 3;
 
             let elevatorRotationCounter = 0;
             let rutterRotationCounter = 0;
@@ -97,18 +107,17 @@ function createScene() {
                             case "W":
                                 if (elevatorRotationCounter > -rotationLimit) {
                                     piperElevator.addRotation(0, 0, -rotationOffset);
-                                    piperBody.addRotation(0, 0, rotationOffset);
                                     elevatorRotationCounter -= rotationOffset;
                                 }
-
+                                piperBody.addRotation(0, 0, rotationOffset)
                                 break;
                             case "s":
                             case "S":
                                 if (elevatorRotationCounter < rotationLimit) {
                                     piperElevator.addRotation(0, 0, rotationOffset);
-                                    piperBody.addRotation(0, 0, -rotationOffset);
                                     elevatorRotationCounter += rotationOffset;
                                 }
+                                piperBody.addRotation(0, 0, -rotationOffset)
                                 break;
                             case "q":
                             case "Q":
@@ -116,6 +125,7 @@ function createScene() {
                                     piperRutter.addRotation(0, 0, rotationOffset);
                                     rutterRotationCounter += rotationOffset;
                                 }
+                                piperBody.addRotation(0, -rotationOffset, 0);
                                 break;
                             case "e":
                             case "E":
@@ -123,25 +133,27 @@ function createScene() {
                                     piperRutter.addRotation(0, 0, -rotationOffset);
                                     rutterRotationCounter -= rotationOffset;
                                 }
+                                piperBody.addRotation(0, rotationOffset, 0);
                                 break;
                             case "a":
                             case "A":
                                 if (aileronRotationCounter < rotationLimit) {
                                     piperAileronPilot.addRotation(0, 0, rotationOffset);
                                     piperAileronPass.addRotation(0, 0, -rotationOffset);
-                                    piperBody.addRotation(-rotationOffset, 0, 0);
                                     aileronRotationCounter += rotationOffset
-
                                 }
+                                piperBody.addRotation(-rotationOffset, 0, 0);
                                 break;
                             case "d":
                             case "D":
                                 if (aileronRotationCounter > -rotationLimit) {
                                     piperAileronPilot.addRotation(0, 0, -rotationOffset);
                                     piperAileronPass.addRotation(0, 0, rotationOffset);
-                                    piperBody.addRotation(rotationOffset, 0, 0);
                                     aileronRotationCounter -= rotationOffset
                                 }
+                                piperBody.addRotation(rotationOffset, 0, 0);
+                                break;
+                            default:
                                 break;
                         }
                         break;
@@ -159,39 +171,57 @@ function createScene() {
                         // case "s":
                         // case "S":
                         //     break;
+                    default:
+                        break;
                 }
+
+
             });
         }
 
-        BABYLON.SceneLoader.ImportMesh("", "Piper/", "Piper_J3.babylon", scene, function (meshes) {
-            piperBody = meshes[0];
-            piperElevator = meshes[1];
-            piperProp = meshes[2];
-            piperRutter = meshes[3];
-            piperAileronPilot = meshes[4];
-            piperAileronPass = meshes[5];
 
-            piperRutter.parent = piperBody;
-            piperElevator.parent = piperBody;
-            piperProp.parent = piperBody;
-            piperAileronPilot.parent = piperBody;
-            piperAileronPass.parent = piperBody;
+        let result = await BABYLON.SceneLoader.ImportMeshAsync("", "Piper/", "Piper_J3.babylon", scene);
+        let meshes = result.meshes;
 
+        piperBody = meshes[0];
+        piperElevator = meshes[1];
+        piperProp = meshes[2];
+        piperRutter = meshes[3];
+        piperAileronPilot = meshes[4];
+        piperAileronPass = meshes[5];
+        piperPositionLightRight = meshes[6];
+        piperPositionLightLeft = meshes[7];
+        piperPositionLightTail = meshes[8];
 
-            setupInitialTranslations();
-            setupPropAnimation();
-            setupControlSurfaceInput();
+        piperRutter.parent = piperBody;
+        piperElevator.parent = piperBody;
+        piperProp.parent = piperBody;
+        piperAileronPilot.parent = piperBody;
+        piperAileronPass.parent = piperBody;
+        piperPositionLightRight.parent = piperBody;
+        piperPositionLightLeft.parent = piperBody;
+        piperPositionLightTail.parent = piperBody;
 
-            camera.lockedTarget = piperBody;
-        });
+        cameraAttachment = BABYLON.MeshBuilder.CreateBox("CameraAttachment", {}, 0);
+        cameraAttachment.isVisible = false;
+
+        camera.lockedTarget = cameraAttachment;
+        camera.rotationOffset = 90;
+
+        piperBody.checkCollisions = true;
+
+        setupInitialTranslations();
+        setupControlSurfaceInput();
+        setupLights();
+        setupAnimations();
     }
 
     function generateTerrain() {
-        let mapSubX = 1000;
-        let mapSubZ = 1000;
-        let seed = 0.1;d
+        let mapSubX = 2000;
+        let mapSubZ = 2000;
+        let seed = 0.1;
         let noiseScale = 0.001;
-        let elevationScale = 150.0;
+        let elevationScale = 250.0;
         noise.seed(seed);
         let mapData = new Float32Array(mapSubX * mapSubZ * 3);
         let mapColors = new Float32Array(mapSubX * mapSubZ * 3); 
@@ -217,11 +247,12 @@ function createScene() {
                     mapColors[xPos + 1] = 0.20; 
                     mapColors[xPos + 2] = 0.0;    
                 }
-                else if(y < 50) {
+                else if(y < 130) {
                     mapColors[xPos] = 0.4;     
                     mapColors[xPos + 1] = 0.27;
                     mapColors[xPos + 2] = 0.0;    
-                } else {
+                } 
+                else {
                     mapColors[xPos] = 0.90;     
                     mapColors[xPos + 1] = 1.00; 
                     mapColors[xPos + 2] = 1.00;    
@@ -231,7 +262,7 @@ function createScene() {
         }
 
 
-        let terrainSub = 50;
+        let terrainSub = 100;
         let params = {
             mapData: mapData,
             mapSubX: mapSubX,
@@ -245,22 +276,64 @@ function createScene() {
         terrainMaterial.specularColor = BABYLON.Color3.Black();
         terrain.mesh.material = terrainMaterial;
 
-        terrain.initialLOD = 100;
+        terrain.initialLOD = 50;
+        terrain.checkCollisions = true;
         terrain.update(true);
 
     }
 
+    function setupParticles() {
+        let emitter = BABYLON.MeshBuilder.CreateBox("Rain emitter", {}, scene);
+        emitter.position.x = piperBody.position.x - 100;
+        emitter.position.y = piperBody.position.y - 10;
+        emitter.parent = piperBody;
+        emitter.isVisible = false;
+        BABYLON.ParticleHelper.CreateAsync("rain", scene).then((set) => {
+            set.systems.forEach((system) => { system.emitter = emitter } )
+            set.systems[0].minLifeTime = 5;
+            set.systems[0].maxLifeTime = 5;
+            delete set.systems[1];
+            set.systems.length = 1;
+            console.log(set);
+            set.start();
+        });
+    }
+
+    function setupSkybox() {
+        let skybox = new BABYLON.MeshBuilder.CreateBox("Skybox", {size: 4096}, scene);
+        let skyboxMaterial = new BABYLON.StandardMaterial("Skybox material", scene);
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/clouds", scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        skybox.material = skyboxMaterial;
+
+        skybox.parent = piperBody;
+    }
+
+    function setupGround() {
+        let ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("GroundHeightMap", "textures/height-map.png", {width: 2048, height: 40960, subdivisions: 10, maxHeight: 2000, updatable:true});
+        ground.position.z += 20480;
+    }
+
     setupScene();
-    importMeshes();
     setupCamera();
     setupLights();
-    setupOrientationLines();
     generateTerrain();
+    // setupGround();
+
+
+    await importAndSetupPiper();
+    // setupSkybox();
+    setupParticles();
+
+
 
     return scene;
 };
 
-function init() {
+async function init() {
     canvas = document.getElementById("renderCanvas");
     engine = new BABYLON.Engine(
         canvas,
@@ -268,7 +341,7 @@ function init() {
         { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false }
     );
 
-    let scene = createScene();
+    let scene = await createScene();
 
     engine.runRenderLoop(function () {
         scene.render();
